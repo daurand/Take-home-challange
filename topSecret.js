a742c38adc2814791ab6e2a35b63bd69651c2294
Daniel Aurand <daurand303@gmail.com>
2023-02-01 11:39:04 -0700

    format comments

--- topSecret.js
+++ topSecret.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
 
 // function finds base SS for driver and location passed in.
-// takes a 2 strings, returns a number.
+// takes in 2 strings as arguments, returns a number.
 function findSS(location, driver) {
   const vowels = 'aeiouAEIOU';
   let baseSS = 0;
 
-  for (const char of driver) { // finds all vowels in driver.
+    // finds all vowels in driver.
+  for (const char of driver) { 
     if (vowels.includes(char)) {
       baseSS++;
     }
   }
 
-  if (location.length % 2 === 0) { // if even.
+  if (location.length % 2 === 0) { // if location.length is even.
     baseSS *= 1.5;
   } else { // if odd.
     baseSS = 1 * (driver.length - baseSS);
@@ -26,12 +27,13 @@ function findSS(location, driver) {
 }
 
 // function reads files, iterates through them, and finds best SS.
-// takes 2 files, console logs total SS and driver/location matches.
+// takes in 2 files as arguments, console logs total SS and driver/location matches.
 function main(locationFile, driverFile) {
 
     // store data in an array.
   const locations = fs.readFileSync(locationFile, 'utf-8').split('\n');
   const drivers = fs.readFileSync(driverFile, 'utf-8').split('\n');
+    // declare storage for output
   const matches = {};
   let totalSS = 0;
 
@@ -46,7 +48,7 @@ function main(locationFile, driverFile) {
         // check if driver has a location already.
       if (!matches[drivers[j]]) {
         const ss = findSS(locations[i], drivers[j]);
-        // find best SS.
+        // find best SS for location.
         if (ss > bestSS) {
           bestDriver = drivers[j];
           bestSS = ss;
@@ -64,6 +66,6 @@ function main(locationFile, driverFile) {
   console.log('Matches:', matches);
 }
 
-// invoc main()
+// invoke main()
 main(process.argv[2], process.argv[3]);
 
